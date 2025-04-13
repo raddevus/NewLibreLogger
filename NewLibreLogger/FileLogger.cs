d4295b2d038ee9cc5e83c157b2e36cc46164535c
raddevus <1radeutsch1@gmail.com>
2025-04-12 22:50:18 -0400

    added file

---
+++ NewLibreLogger/FileLogger.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace NewLibre;
+
+public class FileLogger: Loggable{
+   private String FileName;
+   public FileLogger(){
+      Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff tt")} : FileActivityTracker ctor...");
+   }
+
+   public override bool Configure(){
+      FileName = @$"Path.Combine({Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)},temp,InterfaceStudy.log";
+      return true;
+   }
+
+   public override string StorageTarget{
+      get{ return FileName;}
+   }
+
+   public override bool Write(String message){
+      try{
+          File.AppendAllText(StorageTarget, $"{DateTime.Now.ToLongTimeString()}:   {message} {Environment.NewLine}");
+          return true;
+      }
+      catch(Exception ex){
+         return false;
+      }
+   }
+}
+
