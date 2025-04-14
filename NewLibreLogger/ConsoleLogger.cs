22e7300e5415e687e2cb333e2846a562cc193cda
raddevus <1radeutsch1@gmail.com>
2025-04-14 13:46:18 -0400

    Adding ConsoleLogger so user can log to console too & then later switch to file, db etc.

---
+++ NewLibreLogger/ConsoleLogger.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace NewLibre;
+
+public class ConsoleLogger: Loggable{
+   private String Path = @$"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}";
+   private String FileName = "file.log";
+   private String TargetFile;
+   
+   public ConsoleLogger(string path = null, string filename = null){
+      if (!path.IsNullOrEmpty()){
+         Path = path;
+      }
+      if (!filename.IsNullOrEmpty()){
+         FileName = filename;
+      }
+      Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff tt")} : FileActivityTracker ctor...");
+      Configure();
+   }
+
+   public override bool Configure(){
+      TargetFile = @$"{System.IO.Path.Combine(Path,FileName)}";
+      return true;
+   }
+
+   public override string StorageTarget{
+      get{ return TargetFile;}
+   }
+
+   public override bool Write(String message){
+      try{
+          File.AppendAllText(StorageTarget, $"{DateTime.Now.ToLongTimeString()}:   {message} {Environment.NewLine}");
+          return true;
+      }
+      catch(Exception ex){
+         return false;
+      }
+   }
+}
+
