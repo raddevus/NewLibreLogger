04bc73ea7759af62a0d16786dfa5f54d10f12c5d
raddevus <1radeutsch1@gmail.com>
2025-04-15 10:52:35 -0400

    Added Endpoint Logger so user will be able to log to any URL via a webapi.

---
+++ NewLibreLogger/EndpointLogger.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace NewLibre;
+
+public class EndpointLogger: Loggable{
+   private String Path = @$"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}";
+   private String FileName = "file.log";
+   private String TargetFile;
+   
+   public EndpointLogger(string path = null, string filename = null){
+      if (!path.IsNullOrEmpty()){
+         Path = path;
+      }
+      if (!filename.IsNullOrEmpty()){
+         FileName = filename;
+      }
+      Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff tt")} : FileActivityTracker ctor...");
+      Configure();
+   }
+
+   public override bool Configure(){
+      TargetFile = @$"{System.IO.Path.Combine(Path,FileName)}";
+      return true;
+   }
+
+   public override string StorageTarget{
+      get{ return TargetFile;}
+   }
+
+   public override bool Write(String message){
+      try{
+          File.AppendAllText(StorageTarget, $"{DateTime.Now.ToLongTimeString()}:   {message} {Environment.NewLine}");
+          return true;
+      }
+      catch(Exception ex){
+         return false;
+      }
+   }
+   
+   public async Task WriteAsync(String message){
+      Task.Run(() => {
+         try{
+             File.AppendAllText(StorageTarget, $"{DateTime.Now.ToLongTimeString()}:   {message} {Environment.NewLine}");
+             return true;
+         }
+         catch(Exception ex){
+            return false;
+         }
+      });
+   }
+}
+
