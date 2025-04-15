1d07ee3ea92e45520329f13bdea95ac2acacd8b1
raddevus <1radeutsch1@gmail.com>
2025-04-15 17:03:21 -0400

    Adding endpoint tests

---
+++ NewLibreLogger.Tests/EndpointLoggerTests.cs
@@ -0,0 +1,30 @@
+namespace NewLibreLogger.Tests;
+using NewLibre;
+using System.IO;
+
+public class EndpointLoggerTests{
+
+   [Fact]
+   void WriteMsgWithTimeTest(){
+      ConsoleLogger cl = new();
+      cl.Write("This is the console test.");
+   }
+
+   [Fact]
+   void WriteMsgWithoutTime(){
+      ConsoleLogger cl = new();
+      cl.WriteMsg("This message is written without date/time info.");
+   }
+
+   [Fact]
+   void WriteMsgAsyncNoWaitTest(){
+      ConsoleLogger cl = new();
+      Task.Run (() => cl.WriteAsync("I'm writing async now. No stopping!"));
+   }
+
+   [Fact] 
+   async void WriteMsgAsyncBlockTest(){
+      ConsoleLogger cl = new();
+      await cl.WriteAsync("I'm writing async with BLOCKing!");
+   }
+}
