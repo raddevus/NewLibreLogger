d841043962ea0b2c4112a98682528a5653ea817b
raddevus <1radeutsch1@gmail.com>
2025-04-15 14:37:15 -0400

    Continuing work to get EndpointLogger working.

--- NewLibreLogger/EndpointLogger.cs
+++ NewLibreLogger/EndpointLogger.cs
@@ -1,40 +1,30 @@
 using System;
+using System.Net.Http;
 
 namespace NewLibre;
 
 public class EndpointLogger: Loggable{
-   private String Path = @$"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}";
-   private String FileName = "file.log";
-   private String TargetFile;
+   private String Endpoint;
+   private readonly HttpClient HttpClient;
    
-   public EndpointLogger(string path = null, string filename = null){
-      if (!path.IsNullOrEmpty()){
-         Path = path;
+   public EndpointLogger(String endpoint){
+      if (!endpoint.IsNullOrEmpty()){
+         Endpoint = endpoint;
       }
-      if (!filename.IsNullOrEmpty()){
-         FileName = filename;
-      }
-      Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff tt")} : FileActivityTracker ctor...");
+      Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff tt")} : EndpointLogger ctor...");
       Configure();
    }
 
    public override bool Configure(){
-      TargetFile = @$"{System.IO.Path.Combine(Path,FileName)}";
       return true;
    }
 
-   public override string StorageTarget{
-      get{ return TargetFile;}
+   public override string StorageTarget   {
+      get{ return Endpoint;}
    }
 
    public override bool Write(String message){
-      try{
-          File.AppendAllText(StorageTarget, $"{DateTime.Now.ToLongTimeString()}:   {message} {Environment.NewLine}");
-          return true;
-      }
-      catch(Exception ex){
-         return false;
-      }
+      return true;
    }
    
    public async Task WriteAsync(String message){
